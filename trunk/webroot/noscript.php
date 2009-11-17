366edf8e240320b91b1e07f748d69f229001125f
mmarin <mmarin@f4fcbe23-b6f7-f801-a198-f47f4a01799d>
2009-11-17 22:39:44 +0000

    * Comprobación para ver si el browser tiene habilitado javascript. Closes #50 add 1h

---
+++ trunk/webroot/noscript.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>CyMremun</title>
+</head>
+<body>
+<noscript>
+<h1>Su navegador no soporta javascript.</h1>
+<p>Por favor, habilite javascript y recargue la p&aacute;gina</p>
+</noscript>
+</body>
+</html>
